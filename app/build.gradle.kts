7cbeda5512c6784625b92e877428bb333ad0013b
LadiesMan-two-one-seven <asanagaevskiyi@list.ru>
2026-08-29 14:58:03 +0300

    SubscriptionsScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.converter.kotlinx.serialization)
     implementation(libs.retrofit)
     implementation(libs.coil.compose)
