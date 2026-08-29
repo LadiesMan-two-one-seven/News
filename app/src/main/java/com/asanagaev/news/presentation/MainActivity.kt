fb4d8bca51d4a97fcc16c9a8bf939553bab4f41a
LadiesMan-two-one-seven <asanagaevskiyi@list.ru>
2026-08-29 22:38:17 +0300

    Notifications

--- app/src/main/java/com/asanagaev/news/presentation/MainActivity.kt
+++ app/src/main/java/com/asanagaev/news/presentation/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.asanagaev.news.presentation
 
-import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import com.asanagaev.news.presentation.screen.subscriptions.SubscriptionScreen
 import com.asanagaev.news.presentation.ui.theme.NewsTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,8 +19,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NewsTheme {
+                val permissionLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestPermission(),
+                    onResult = {}
+                )
                 SubscriptionScreen(
-                    onNavigateToSettings = {}
+                    onNavigateToSettings = {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                            permissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+                        }
+                    }
                 )
             }
         }
