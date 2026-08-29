bd66a48869058e04c05604de0a289cb72cbb75e7
LadiesMan-two-one-seven <asanagaevskiyi@list.ru>
2026-08-29 17:19:21 +0300

    Hilt with Workers (https://developer.android.com/develop/background-work/background-tasks/persistent/configuration/custom-configuration)

--- app/src/main/java/com/asanagaev/news/data/dackground/RefreshDataWorker.kt
+++ app/src/main/java/com/asanagaev/news/data/dackground/RefreshDataWorker.kt
@@ -2,13 +2,17 @@ package com.asanagaev.news.data.dackground
 
 import android.content.Context
 import android.util.Log
+import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.asanagaev.news.domain.usecase.UpdateSubscribedArticlesUseCase
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 
-class RefreshDataWorker(
-    context: Context,
-    workerParameters: WorkerParameters,
+@HiltWorker
+class RefreshDataWorker @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted workerParameters: WorkerParameters,
     private val updateSubscribedArticlesUseCase: UpdateSubscribedArticlesUseCase
 ) : CoroutineWorker(context, workerParameters) {
 
